04b17ab2aba85acddb2636a849fc76654e52ba78
Caio da Silva <66576487+thecaiodasilva@users.noreply.github.com>
2022-10-22 00:09:53 -0300

    Add files via upload

---
+++ DIO/Primeiros passos com JavaScipt/variaveis e operadores.js
@@ -0,0 +1,8 @@
+const precoCombustivel = 3.69;
+const kmPorLitros = 9;
+const distancia = 369;
+
+const litroConsumidos = distancia/kmPorLitros;
+const valorGasto = litroConsumidos*precoCombustivel
+
+console.log(valorGasto)
