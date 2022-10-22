1576ed36cdffd43f892d7eab6e38fb8fdcc827a4
Caio da Silva <66576487+thecaiodasilva@users.noreply.github.com>
2022-10-22 01:39:46 -0300

    Update condicionais.js

--- DIO/Primeiros passos com JavaScipt/condicionais.js
+++ DIO/Primeiros passos com JavaScipt/condicionais.js
@@ -1,13 +1,3 @@
-// Faça um programa para calcular o valor de uma viagem.
-
-// Você terá 5 variáveis. Sendo elas:
-//  1 - Preço do etanol;
-//  2 - Preço do gasolina;
-//  3 - O tipo de combustível que está no seu carro;
-//  4 - Gasto médio de combustível do carro por KM;
-//  5 - Distância em KM da viagem;
-
-// Imprima no console o valor que será gasto para realizar esta viagem.
 const precoEtanol = 3.69;
 const precoGasolina = 6.66;
 const kmPorLitros = 9;
